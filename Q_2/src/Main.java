21e354b7846df0b7f036cbf9049e9c01cf982986
2241001030 <gentlecode45@gmail.com>
2024-02-20 23:50:26 +0530

    20-02-2024

--- Q_2/src/Main.java
+++ Q_2/src/Main.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
         Student[] s= new Student[5];
         for(int i =0;i<5;i++){
             s[i]=new Student();
