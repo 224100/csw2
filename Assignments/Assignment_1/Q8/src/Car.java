259e55df3b72f107280a61ed8d4bf6096c964d73
2241001030 <160648712+2241001030@users.noreply.github.com>
2024-02-28 15:40:22 +0530

    28-02-2024

---
+++ Assignments/Assignment_1/Q8/src/Car.java
@@ -0,0 +1,16 @@
+public class Car implements Vehicle{
+    @Override
+    public void accelerate(int a) {
+
+    }
+
+    @Override
+    public void accelerate(double a, double b) {
+        
+    }
+
+    @Override
+    public void brake() {
+
+    }
+}
---
+++ Assignments/Assignment_1/Q8/src/Vehicle.java
@@ -0,0 +1,6 @@
+public interface Vehicle {
+    void accelerate(int a);
+    void accelerate(double a,double b);
+    void brake();
+
+}
