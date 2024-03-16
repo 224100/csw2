e5f06e81ad0a89d442b58768c8fe8f56887d39b1
2241001030 <160648712+2241001030@users.noreply.github.com>
2024-03-16 23:07:09 +0530

    16-03-2024

--- Assignments/Assignment_2_1/A2Q4/src/Main.java
+++ Assignments/Assignment_2_1/A2Q4/src/Main.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        Student[] s =new Student[5];
-        for(int i =0 ;i<5;i++){
-            System.out.println("Enter Students name rollno and total marks");
-            s[i]=new Student(sc.nextLine(),sc.nextInt(),sc.nextDouble());
-
+        Scanner sc = new Scanner(System.in);
+        Student[] students = new Student[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter student's name, roll number, and total marks:");
+            students[i] = new Student(sc.nextLine(), sc.nextInt(), sc.nextDouble());
+            sc.nextLine();
         }
-        System.out.println("Enter the student to be searched :");
-        Student s1 =new Student(sc.nextLine(),sc.nextInt(),sc.nextDouble());
-        for(int i =0;i<5;i++){
-            if(s1.compareTo)
+
+        System.out.println("Enter the student to be searched (name, roll number, total marks):");
+        Student searchStudent = new Student(sc.nextLine(), sc.nextInt(), sc.nextDouble());
+
+        for (Student student : students) {
+            if (searchStudent.compareTo(student) == 0) {
+                System.out.println("Student found:");
+                System.out.println(student);
+                break;
+            }
         }
     }
 }
