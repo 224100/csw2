8c7ae5e62c21ea12ccbe17c105db17e29e634da7
2241001030 <160648712+2241001030@users.noreply.github.com>
2024-04-03 16:09:31 +0530

    3-4-2024

--- Assignments/Assignment_3/src/Q3/main.java
+++ Assignments/Assignment_3/src/Q3/main.java
@@ -2,6 +2,18 @@ package Q3;
 
 public class main {
     public static void main(String[] args) {
+        String s =null;
+        try{
+            System.out.println(s.length());
+        }
+        catch(CustomNullPointerException e){
+            e =new CustomNullPointerException("Ohhhhh noooo!");
+        }
 
+    }
+}
+class CustomNullPointerException extends NullPointerException{
+    CustomNullPointerException(String s){
+        super(s);
     }
 }
--- test/src/Main.java
+++ test/src/Main.java
@@ -1,13 +1,38 @@
-import java.util.ArrayList;
+
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> a =new ArrayList<>();
-        a.add("hello");
-        a.add("abc");
-        a.add(1,"hello");
-        System.out.println(a);
-        a.removeLast();
-        System.out.println(a);
+    static void f1(){
+        String s ="Hello";
+        System.gc();
     }
+    public static void main(String[] args) {
+       f1();
+       Runtime r = Runtime.getRuntime();
+       String s =null;
+
+       System.gc();
+        System.out.println(r.totalMemory());
+        System.out.println(r.freeMemory());
+        Sample s1 =new Sample("1");
+        //Anoynomus
+        new Sample("Hello");
+        System.gc();
+        //Re-Reference
+        Sample s2 =s1;
+        System.gc();
+        System.out.println(s2);
+        // null objects
+        Sample s3 =null;
+        System.gc();
+
+
+    }
+}
+class Sample{
+    String s;
+    public Sample(String s)
+    {
+        this.s =s;
+    }
+
 }
