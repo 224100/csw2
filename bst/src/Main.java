8b399fa4636e70ded2401b684af218f130350623
224100 <gentlecode45@gmail.com>
2024-05-01 17:32:13 +0530

    1-5

--- bst/src/Main.java
+++ bst/src/Main.java
@@ -1,68 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-    }
-}
-class bstnode {
-    bstnode right;
-    bstnode left;
-    int data;
-}
-class bst{
-    bstnode root;
-    public bst(){
-        root = null;
-    }
-    public void insert(int data){
-        bstnode newnode = new bstnode();
-        newnode.data = data;
-        if(root == null){
-            root = newnode;
-        }
-        else{
-            bstnode current = root;
-            bstnode parent;
-            while(true){
-                parent = current;
-                if(data < current.data){
-                    current = current.left;
-                    if(current == null){
-                        parent.left = newnode;
-                        return;
-                    }
-                }
-                else{
-                    current = current.right;
-                    if(current == null){
-                        parent.right = newnode;
-                        return;
-                    }
-                }
-            }
-        }
-    }
-    public void inorder(bstnode root){
-        if(root != null){
-            inorder(root.left);
-            System.out.print(root.data + " ");
-            inorder(root.right);
-        }
-    }
-    public void preorder(bstnode root){
-        if(root != null){
-            System.out.print(root.data + " ");
-            preorder(root.left);
-            preorder(root.right);
-        }
-    }
-    public void postorder(bstnode root){
-        if(root != null){
-            postorder(root.left);
-            postorder(root.right);
-            System.out.print(root.data + " ");
-        }
-    }
-    public void delete(int data){
+        bst b = new bst();
+        int x =1;
+        while(x!=0){
+            System.out.println("Enter 1 to insert,\n2 to r_insert,\n3 to delete,\n4 to inorder,\n5 to preorder,\n6 to postorder,\n7 to find max , \n8 to find min, \n0 to exit");
+            Scanner sc = new Scanner(System.in);
+            x = sc.nextInt();
+            switch (x){
+                case 1:
+                    System.out.println("Enter the data to insert");
+                    int data = sc.nextInt();
+                    b.insert(data);
+                    break;
+                case 2:
+                    System.out.println("Enter the data to insert");
+                    int data1 = sc.nextInt();
+                    b.insert_recursive(data1);
+                    break;
+                case 3:
+                    System.out.println("Enter the data to delete");
+                    int data2 = sc.nextInt();
+                    b.delete(data2);
+                    break;
+                case 4:
+                    b.inorder(b.root);
+                    System.out.println();
+                    break;
+                case 5:
+                    b.preorder(b.root);
+                    System.out.println();
+                    break;
+                case 6:
+                    b.postorder(b.root);
+                    System.out.println();
+                    break;
+                case 7:
+                    System.out.println("Max node is: " + b.max_node(b.root));
+                    break;
+                case 8:
+                    System.out.println("Min node is: " + b.min_node(b.root));
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid input");
 
+            }
+
+        }
     }
 }
---
+++ bst/src/bst.java
@@ -0,0 +1,95 @@
+class bst{
+    bstnode root;
+    public bst(){
+        root = null;
+    }
+    public int max_node(bstnode root){
+        if(root.right == null){
+            return root.data;
+        }
+        else{
+            return max_node(root.right);
+        }
+    }
+    public int min_node(bstnode root){
+        if(root.left == null){
+            return root.data;
+        }
+        else{
+            return min_node(root.left);
+        }
+    }
+    public void insert_recursive(int data){
+        root = insert_recursive(root, data);
+    }
+    public bstnode insert_recursive(bstnode c, int data){
+        if(c == null){
+            c = new bstnode();
+            c.data = data;
+        }
+        else{
+            if(data < c.data)
+                c.left = insert_recursive(c.left, data);
+            else
+                c.right = insert_recursive(c.right, data);
+        }
+        return c;
+    }
+    public void insert(int data){
+        bstnode newnode = new bstnode();
+        newnode.data = data;
+        if(root == null){
+            root = newnode;
+        }
+        else{
+            bstnode current = root;
+            bstnode parent;
+            while(true){
+                parent = current;
+                if(data < current.data){
+                    current = current.left;
+                    if(current == null){
+                        parent.left = newnode;
+                        return;
+                    }
+                }
+                else{
+                    current = current.right;
+                    if(current == null){
+                        parent.right = newnode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    public void inorder(bstnode root){
+        if(root != null){
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+    public void preorder(bstnode root){
+        if(root != null){
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    public void postorder(bstnode root){
+        if(root != null){
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+    public void delete(int data){
+
+    }
+}
+class bstnode {
+    bstnode right;
+    bstnode left;
+    int data;
+}
