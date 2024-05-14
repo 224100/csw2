ce6b999b0cd981f9438b318e60cca48816b4cf25
224100 <gentlecode45@gmail.com>
2024-05-14 13:49:05 +0530

    14

--- lambda_expression/src/Main.java
+++ lambda_expression/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
-
+interface intformat{
+    int run(int n);
+}
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>();
@@ -7,5 +9,12 @@ public class Main {
         a.add(20);
         a.add(55);
         a.add(45);
+        a.forEach((n) -> System.out.println(n));
+        intformat t = (b) -> b + 100;
+        System.out.println(t.run(a.get(0)));
+    }
+    void printFormat(int a , intformat t){
+        int x = t.run(a);
+        System.out.println(x);
     }
 }
