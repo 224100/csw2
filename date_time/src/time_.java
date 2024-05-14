9200ebbb843fe8f96efa31e16f02ab480088445b
224100 <160648712+224100@users.noreply.github.com>
2024-05-14 17:24:26 +0530

    14-5

--- date_time/src/time_.java
+++ date_time/src/time_.java
@@ -1,8 +1,11 @@
-import java.time.LocalTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class time_ {
     public static void main(String[] args) {
-        LocalTime t = LocalTime.now();
-        System.out.println(t);
+
+        DateTimeFormatter f =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime t = LocalDateTime.now();
+        System.out.println(t.format(f));
     }
 }
