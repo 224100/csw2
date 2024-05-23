cabd82fafaae04166f8c95240b8eae00aa985fc8
224100 <160648712+224100@users.noreply.github.com>
2024-05-23 17:30:01 +0530

    23-5

--- Assignments/Assignment_9_multi_threading/A9Q6/src/Main.java
+++ Assignments/Assignment_9_multi_threading/A9Q6/src/Main.java
@@ -30,9 +30,28 @@ class PrimeG implements Runnable {
 public class Main {
     public static void main(String[] args) {
 
-            
-            Thread t = new Thread(new PrimeG(1, 10));
-            t.start();
+        PrimeG[] p = new PrimeG[5];
+        Thread[] t = new Thread[5];
+        int b = 1;
+        int e=101;
+        int interval = (e-b)/5;
+        int x=interval;
+        for(int i =0;i<5;i++){
+
+            if(x>e)
+            {
+                p[i] = new PrimeG(b,e);
+                t[i] = new Thread(p[i]);
+                t[i].start();
+            }
+            else{
+                p[i] = new PrimeG(b,x);
+                t[i] = new Thread(p[i]);
+                t[i].start();
+            }
+            b=x+1;
+            x+=interval;
+        }
         }
     }
 
